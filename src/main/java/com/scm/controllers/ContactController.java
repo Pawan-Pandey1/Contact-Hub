ce5493d1c1eb2dd833bf79967f67bb1ddd1ce016
Pawan Pandey <pawanpandey.3002@gmail.com>
2025-05-06 09:00:58 +0530

    contact form validation
     Please enter the commit message for your changes. Lines starting

--- src/main/java/com/scm/controllers/ContactController.java
+++ src/main/java/com/scm/controllers/ContactController.java
@@ -1,54 +1,89 @@
 package com.scm.controllers;
 
+import java.util.*;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.scm.entities.Contact;
 import com.scm.entities.User;
 import com.scm.forms.ContactForm;
+// import com.scm.forms.ContactSearchForm;
+import com.scm.helpers.AppConstants;
 import com.scm.helpers.Helper;
+import com.scm.helpers.Message;
+import com.scm.helpers.MessageType;
 import com.scm.services.ContactService;
+// import com.scm.services.ImageService;
 import com.scm.services.UserService;
 
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
-
 @RequestMapping("/user/contacts")
 public class ContactController {
 
+    private Logger logger = org.slf4j.LoggerFactory.getLogger(ContactController.class);
+
     @Autowired
-    private ContactService ContactService;
+    private ContactService contactService;
+
+    // @Autowired
+    // private ImageService imageService;
 
     @Autowired
     private UserService userService;
 
     @RequestMapping("/add")
-    //Add contact page handler
-    public String addContactView(Model model){
+    // add contact page: handler
+    public String addContactView(Model model) {
+        ContactForm contactForm = new ContactForm();
 
-        ContactForm contactForm=new ContactForm();
-        contactForm.setName("Pawan Pandey");
         contactForm.setFavorite(true);
         model.addAttribute("contactForm", contactForm);
         return "user/add_contact";
     }
 
-    @RequestMapping(value ="/add", method = RequestMethod.POST)
-    public String saveContact(@ModelAttribute ContactForm contactForm,Authentication authentication ){
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String saveContact(@Valid @ModelAttribute ContactForm contactForm, BindingResult result,
+            Authentication authentication, HttpSession session) {
 
-        //process the form data
-        String username=Helper.getEmailOfLoggedInUser(authentication);
+        // process the form data
 
-        //first convert the form to contact
+        // 1 validate form
+
+        if (result.hasErrors()) {
+
+            result.getAllErrors().forEach(error -> logger.info(error.toString()));
+
+            session.setAttribute("message", Message.builder()
+                    .content("Please correct the following errors")
+                    .type(MessageType.red)
+                    .build());
+            return "user/add_contact";
+        }
+
+        String username = Helper.getEmailOfLoggedInUser(authentication);
+        // form ---> contact
+
         User user = userService.getUserByEmail(username);
+        // 2 process the contact picture
 
+        // image process
 
-        Contact contact =new Contact();
+        // uplod karne ka code
+        Contact contact = new Contact();
         contact.setName(contactForm.getName());
         contact.setFavorite(contactForm.isFavorite());
         contact.setEmail(contactForm.getEmail());
@@ -59,11 +94,27 @@ public class ContactController {
         contact.setLinkedInLink(contactForm.getLinkedInLink());
         contact.setWebsiteLink(contactForm.getWebsiteLink());
 
+        // if (contactForm.getContactImage() != null && !contactForm.getContactImage().isEmpty()) {
+        //     String filename = UUID.randomUUID().toString();
+        //     String fileURL = imageService.uploadImage(contactForm.getContactImage(), filename);
+        //     contact.setPicture(fileURL);
+        //     contact.setCloudinaryImagePublicId(filename);
 
-        ContactService.save(contact);
+        // }
+        //contactService.save(contact);
+        System.out.println(contactForm);
 
-        System.out.println(
-            contactForm);
+        // 3 set the contact picture url
+
+        // 4 `set message to be displayed on the view
+
+        session.setAttribute("message",
+                Message.builder()
+                        .content("You have successfully added a new contact")
+                        .type(MessageType.green)
+                        .build());
+
         return "redirect:/user/contacts/add";
+
     }
 }
